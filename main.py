150bdb86d2d680b58be364ab6745537ebc61d2ce
cub_rub <serg64spb@gmail.com>
2023-02-28 23:09:33 +0300

    Начальное меню, UI, стрельба врагов

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = (SCREEN_SIZE[0] - self.rect[2]) // 2
         self.rect.y = SCREEN_SIZE[1] - self.rect[3] - 32
         self.bullet = pygame.sprite.GroupSingle()
+        self.lives = 3
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -47,20 +48,48 @@ class Player(pygame.sprite.Sprite):
 
 
 class Bullet(pygame.sprite.Sprite):
-    image = load_image('bullet.png')
-    image = pygame.transform.scale(image, (4, 16))
-
-    def __init__(self, *groups, x, y, direction):
+    def __init__(self, *groups, image='bullet_1x1.png', x, y, direction):
         super().__init__(*groups)
+        self.frames = []
+        sheet = load_image(image)
+        self.cut_sheet(sheet, int(image[-7]), int(image[-5]))
+        self.cur_frame = 0
+        self.frame_count = 0
+
+        self.image = pygame.transform.scale(self.frames[self.cur_frame],
+                                            (self.frames[self.cur_frame].get_rect()[2] * 4,
+                                             self.frames[self.cur_frame].get_rect()[3] * 4))
+
         self.rect = self.image.get_rect()
         self.rect.x = x + 24
-        self.rect.y = y - 16
+        self.rect.y = y - (16 * direction)
         self.direction = direction
 
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
     def update(self):
-        self.rect.y -= 10 * self.direction
-        if self.rect.y < 0:
+        if self.direction == -1:
+            self.rect.y += 6
+        else:
+            self.rect.y -= 10
+        if not SCREEN_SIZE[0] >= self.rect.y >= 0:
             self.kill()
+
+        if self.frame_count == 5:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.frame_count = 0
+            self.image = pygame.transform.scale(self.frames[self.cur_frame],
+                                                (self.frames[self.cur_frame].get_rect()[2] * 4,
+                                                 self.frames[self.cur_frame].get_rect()[3] * 4))
+            self.rect = self.image.get_rect().move(self.rect.x, self.rect.y)
+        self.frame_count += 1
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -74,7 +103,7 @@ class Enemy(pygame.sprite.Sprite):
 
         self.move_frame_count = 0
         self.move_count = 8
-        self.move_freq = 28
+        self.move_freq = 40
         self.direction = 1
 
         self.image = pygame.transform.scale(self.frames[self.cur_frame],
@@ -83,6 +112,8 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x * 4
         self.rect.y = y * 4
+
+        self.bullet = pygame.sprite.GroupSingle()
 
     def cut_sheet(self, sheet, columns, rows):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
@@ -116,29 +147,56 @@ class Enemy(pygame.sprite.Sprite):
                                                  self.frames[self.cur_frame].get_rect()[3] * 4))
             self.rect = self.image.get_rect().move(self.rect.x, self.rect.y)
 
+        self.bullet.update()
+
+    def shoot(self):
+        if not self.bullet.sprite:
+            images = ['bullet1_1x5.png', 'bullet2_4x1.png']
+            self.bullet.add(Bullet(image=random.choice(images), x=self.rect.x, y=self.rect.bottom, direction=-1))
+
 
 def title_screen():
-    screen.fill((0, 0, 0))
-
-    font_title = pygame.font.Font('assets/Retro_Gaming.ttf', 48)
-    space_text = font_title.render('SPACE', True, (255, 255, 255))
-    invaders_text = font_title.render('INVADERS', True, (30, 254, 30))
-    space_text_rect = space_text.get_rect()
-    invaders_text_rect = invaders_text.get_rect()
-    space_text_rect.center = (SCREEN_SIZE[0] // 2 - 160, SCREEN_SIZE[1] // 4)
-    invaders_text_rect.center = (SCREEN_SIZE[0] // 2 + 100, SCREEN_SIZE[1] // 4)
-    screen.blit(space_text, space_text_rect)
-    screen.blit(invaders_text, invaders_text_rect)
-    font_text = pygame.font.Font('assets/Retro_Gaming.ttf', 22)
-    score_text = font_title.render('SPACE INVADERS', True, (255, 255, 255))
-
     while True:
+        screen.fill((0, 0, 0))
+
+        font_title = pygame.font.Font('assets/Retro_Gaming.ttf', 48)
+        space_text = font_title.render('SPACE', True, (255, 255, 255))
+        invaders_text = font_title.render('INVADERS', True, (30, 254, 30))
+        space_text_rect = space_text.get_rect()
+        invaders_text_rect = invaders_text.get_rect()
+        space_text_rect.center = (SCREEN_SIZE[0] // 2 - 160, SCREEN_SIZE[1] // 4)
+        invaders_text_rect.center = (SCREEN_SIZE[0] // 2 + 100, SCREEN_SIZE[1] // 4)
+        screen.blit(space_text, space_text_rect)
+        screen.blit(invaders_text, invaders_text_rect)
+
+        font = pygame.font.Font('assets/Retro_Gaming.ttf', 33)
+        score_text = font.render('HI-SCORE', True, (255, 255, 255))
+        score_text_rect = score_text.get_rect()
+        score_text_rect.x += 10
+        score_text_rect.y += 5
+        screen.blit(score_text, score_text_rect)
+
+        start_font = pygame.font.Font('assets/Retro_Gaming.ttf', 36)
+        start_text = start_font.render('START', True, (255, 255, 255))
+        start_text_select = start_font.render('START', True, (30, 254, 30))
+        start_text_rect = start_text.get_rect()
+        start_text_rect.center = (SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 6 * 4)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type in (pygame.KEYDOWN, pygame.MOUSEBUTTONDOWN):
-                return
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                if start_text_rect.collidepoint(*pos):
+                    return
+
+        pos = pygame.mouse.get_pos()
+        if start_text_rect.collidepoint(*pos):
+            screen.blit(start_text_select, start_text_rect)
+        else:
+            screen.blit(start_text, start_text_rect)
+
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -148,31 +206,76 @@ class Game:
         self.player = Player()
         self.player_group = pygame.sprite.GroupSingle(self.player)
         self.enemies = pygame.sprite.Group()
+        self.shooting = pygame.sprite.Group()
         self.score = 0
         for i in range(11):
-            self.enemies.add(Enemy(image='squid_2x4.png', x=28 + 16 * i, y=20))
-            self.enemies.add(Enemy(image='enemy_2x4.png', x=27 + 16 * i, y=20 + 16))
-            self.enemies.add(Enemy(image='enemy_2x4.png', x=27 + 16 * i, y=20 + 16 * 2))
-            self.enemies.add(Enemy(image='brute_2x5.png', x=26 + 16 * i, y=20 + 16 * 3))
-            self.enemies.add(Enemy(image='brute_2x5.png', x=26 + 16 * i, y=20 + 16 * 4))
+            Enemy(self.enemies, self.shooting, image='squid_2x4.png', x=28 + 16 * i, y=30)
+            self.enemies.add(Enemy(image='enemy_2x4.png', x=27 + 16 * i, y=30 + 16))
+            self.enemies.add(Enemy(image='enemy_2x4.png', x=27 + 16 * i, y=30 + 16 * 2))
+            self.enemies.add(Enemy(image='brute_2x5.png', x=26 + 16 * i, y=30 + 16 * 3))
+            self.enemies.add(Enemy(image='brute_2x5.png', x=26 + 16 * i, y=30 + 16 * 4))
         self.all_game_groups = [self.player_group, self.enemies]
+        self.lives_icon = None
 
     def detect_collision(self):
         if self.player.bullet.sprite:
             for enemy in self.enemies:
                 if self.player.bullet.sprite.rect.colliderect(enemy.rect):
                     self.player.bullet.sprite.kill()
-                    self.score += 100
+                    self.score += 10
                     enemy.kill()
                     break
+        for sprite in self.shooting.sprites():
+            if sprite.bullet.sprite:
+                if sprite.bullet.sprite.rect.colliderect(self.player.rect):
+                    sprite.bullet.sprite.kill()
+                    self.player.lives -= 1
+
+    def shoot(self):
+        random.choice(self.shooting.sprites()).shoot()
 
     def update(self):
         self.player.update()
         self.enemies.update()
         self.detect_collision()
+
+
+    def draw(self):
+        font = pygame.font.Font('assets/Retro_Gaming.ttf', 33)
+        score_text = font.render('SCORE', True, (255, 255, 255))
+        score_text_rect = score_text.get_rect()
+        score_text_rect.x += 10
+        score_text_rect.y += 5
+        screen.blit(score_text, score_text_rect)
+
+        score_num_text = font.render(str(self.score).zfill(6), True, (255, 255, 255))
+        score_num_rect = score_num_text.get_rect()
+        score_num_rect.x += 15
+        score_num_rect.y += 38
+        screen.blit(score_num_text, score_num_rect)
+
+        lives_text = font.render('LIVES', True, (255, 255, 255))
+        lives_rect = lives_text.get_rect()
+        lives_rect.x += 500
+        lives_rect.y += 5
+        screen.blit(lives_text, lives_rect)
+
+        self.lives_icon = pygame.sprite.Group()
+
+        for i in range(1, self.player.lives + 1):
+            image = load_image('lives.png')
+            image = pygame.transform.scale(image,  (image.get_rect().w * 2, image.get_rect().h * 2))
+            rect = image.get_rect()
+            rect.x += 600 + 56 * i
+            rect.y += 10
+            screen.blit(image, rect)
+
+
         for group in self.all_game_groups:
             group.draw(screen)
         self.player.bullet.draw(screen)
+        for sprite in self.shooting.sprites():
+            sprite.bullet.draw(screen)
 
 
 if __name__ == '__main__':
@@ -185,15 +288,20 @@ if __name__ == '__main__':
 
     game = Game()
 
-    ENEMYSHOOT = pygame.USEREVENT + 1
+    ENEMYSHOOT = pygame.USEREVENT
     pygame.time.set_timer(ENEMYSHOOT, 800)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == ENEMYSHOOT:
+                game.shoot()
+                pygame.time.set_timer(ENEMYSHOOT, random.randint(1, 1600))
         screen.fill((0, 0, 0))
         game.update()
+
+        game.draw()
 
         pygame.display.flip()
         clock.tick(FPS)
